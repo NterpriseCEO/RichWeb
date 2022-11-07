40700b538d964b0bc2a3caf42165fa78f0311dde
NterpriseCEO <nuzumalex@gmail.com>
2022-11-07 12:43:51 +0000

    The original javascript from my lab 1 solution

---
+++ lab3/problem-1/index.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+	const noteInput = document.querySelector('#note-input');
+	const selectNoteColour = document.querySelector('#select-note-colour');
+	const createNoteButton = document.querySelector('#create-note-button');
+
+	const template = document.querySelector("template");
+
+	createNoteButton.addEventListener('click', function () {
+		if(noteInput.value) {
+			const clone = template.content.cloneNode(true);
+			const note = clone.querySelector('.note');
+			const noteContents = clone.querySelector('.note-contents');
+			const deleteNoteButton = clone.querySelector('.delete-note-button');
+			const editNoteButton = clone.querySelector('.edit-note-button');
+			const cancelNoteEditButton = clone.querySelector('.cancel-note-edit-button');
+
+			note.style.backgroundColor = selectNoteColour.value;
+			noteContents.innerHTML = noteInput.value;
+
+			deleteNote(deleteNoteButton, note);
+			editNote(editNoteButton, noteContents, cancelNoteEditButton);
+
+			document.body.appendChild(clone);
+			noteInput.value = '';
+		}
+	});
+
+	function deleteNote(deleteNoteButton, note) {
+		deleteNoteButton.addEventListener('click', function () {
+			note.remove();
+		});
+	}
+
+	function editNote(editNoteButton, noteContents, cancelNoteEditButton) {
+		const oldContents = noteContents.innerHTML;
+		let isEditing = false;
+		editNoteButton.addEventListener('click', function () {
+			isEditing = !isEditing;
+			isEditing ? cancelNoteEditButton.classList.remove('hidden') : cancelNoteEditButton.classList.add('hidden');
+			editNoteButton.innerHTML = isEditing ? 'Save' : 'Edit note';
+
+			noteContents.contentEditable = isEditing;
+			noteContents.focus();
+		});
+
+		cancelNoteEditButton.addEventListener('click', function () {
+			isEditing = false;
+			noteContents.contentEditable = false;
+			editNoteButton.innerHTML = 'Edit note';
+			cancelNoteEditButton.classList.add('hidden');
+			noteContents.innerHTML = oldContents;
+		});
+	}
+}
