7579686469b9025ef0b18907145af2ac65d077f0
NterpriseCEO <nuzumalex@gmail.com>
2022-11-07 13:44:35 +0000

    UI for the countdown timer

--- lab3/problem-2/index.js
+++ lab3/problem-2/index.js
@@ -2,7 +2,9 @@ window.onload = function() {
 	//rxjs timer every second
 	let countFrom = 100;
 	Rx.Observable.timer(countFrom, 1000)
-		.map(i => countFrom - i).subscribe((counter) => {
+		.map(i => countFrom - i)
+		.takeWhile(i => i >= 0)
+		.subscribe((counter) => {
 		console.log(counter);
 	});
 }
