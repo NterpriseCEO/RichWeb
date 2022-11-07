d9ff6e7c995e0d51987e320f6dbfe93b8b3acc41
NterpriseCEO <nuzumalex@gmail.com>
2022-11-07 22:47:50 +0000

    Added a click event to the start countdown button

--- lab3/problem-2/index.js
+++ lab3/problem-2/index.js
@@ -1,10 +1,12 @@
 window.onload = function() {
-	//rxjs timer every second
-	let countFrom = 100;
-	Rx.Observable.timer(countFrom, 1000)
-		.map(i => countFrom - i)
-		.takeWhile(i => i >= 0)
-		.subscribe((counter) => {
-		console.log(counter);
+	let countdownButton = document.getElementById('start-countdown');
+	Rx.Observable.fromEvent(countdownButton, 'click').subscribe(() => {
+		let countFrom = 100;
+		Rx.Observable.timer(countFrom, 1000)
+			.map(i => countFrom - i)
+			.takeWhile(i => i >= 0)
+			.subscribe((counter) => {
+			console.log(counter);
+		});
 	});
 }
