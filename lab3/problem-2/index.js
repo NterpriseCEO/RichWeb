460ea9873ef818e6607ebfa9edec0ff04c3746a4
NterpriseCEO <nuzumalex@gmail.com>
2022-11-07 23:52:00 +0000

    Countdown now counts down in seconds

--- lab3/problem-2/index.js
+++ lab3/problem-2/index.js
@@ -1,11 +1,36 @@
 window.onload = function() {
+	let hoursInput = document.getElementById("hours-input"),
+		minutesInput = document.getElementById("minutes-input"),
+		secondsInput = document.getElementById("seconds-input");
+
+	let countdown;
+
+	let cancelCountdown = false;
+
 	let countdownButton = document.getElementById('start-countdown');
-	Rx.Observable.fromEvent(countdownButton, 'click').subscribe(() => {
-		let countFrom = 100;
-		Rx.Observable.timer(countFrom, 1000)
+	let countdownEventListener = Rx.Observable.fromEvent(countdownButton, 'click');
+	countdownEventListener.subscribe(() => {
+
+		// cancel rxjs observable cancel
+		if (countdown) {
+			cancelCountdown = true;
+		}
+
+		let hours = isNaN(hoursInput.value) ? 0 : parseInt(hoursInput.value*60*60),
+			minutes = isNaN(parseInt(minutesInput.value)) ? 0 : parseInt(minutesInput.value*60),
+			seconds = isNaN(parseInt(secondsInput.value)) ? 0 : parseInt(secondsInput.value);
+
+		let countFrom = hours + minutes + seconds;
+
+		cancelCountdown = false;
+
+		countdown = Rx.Observable
+			.timer(countFrom, 1000)
 			.map(i => countFrom - i)
-			.takeWhile(i => i >= 0)
-			.subscribe((counter) => {
+			.takeUntil(countdownEventListener)
+			.takeWhile(i => i >= 0);
+
+		countdown.subscribe((counter) => {
 			console.log(counter);
 		});
 	});
