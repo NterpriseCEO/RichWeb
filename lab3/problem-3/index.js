866addf1e9d459f685946bd2b19c0b163a98089b
NterpriseCEO <nuzumalex@gmail.com>
2022-11-14 11:34:46 +0000

    Can now add child notes

--- lab3/problem-3/index.js
+++ lab3/problem-3/index.js
@@ -5,6 +5,8 @@ window.onload = function() {
 	Rx.Observable.fromEvent(addNoteButton, 'click').subscribe(() => {
 		let noteValue = noteInput.value;
 
+		noteInput.value = "";
+
 		if (noteValue) {
 			let note = new Note(noteValue);
 			note.addNote();
@@ -18,19 +20,47 @@ class Note {
 
 	#parent = null;
 
+	#parentNoteTemplate;
+	#clone;
+
 	constructor(note, parent) {
 		this.#note = note;
+
+		if(parent) {
+			this.#parent = parent;
+		}
+
+		this.#parentNoteTemplate = document.getElementById((parent ? "child" : "parent") + "-note");
+		this.#clone = this.#parentNoteTemplate.content.cloneNode(true);
 	}
 
 	addNote() {
 		//reference the parent-note template
-		let parentNoteTemplate = document.getElementById("parent-note"),
-			clone = parentNoteTemplate.content.cloneNode(true),
-			noteDiv = clone.querySelector(".note"),
-			noteText = clone.querySelector(".note-text");
+		let noteDiv = this.#clone.querySelector(".note"),
+			noteText = this.#clone.querySelector(".note-text");
 
 		noteText.innerHTML = this.#note;
 
 		document.body.appendChild(noteDiv);
+
+		this.listenForAddChildNote(noteDiv);
+	}
+
+	listenForAddChildNote(noteDiv) {
+		//listen for the add child note button
+		//create a new note
+		//add the new note to the parent note
+		let addChildNoteButton = noteDiv.querySelector(".add-child-note"),
+			childNoteInput = noteDiv.querySelector(".child-note-input");
+		Rx.Observable.fromEvent(addChildNoteButton, 'click').subscribe(() => {
+			let childNoteValue = childNoteInput.value;
+			
+			childNoteInput.value = "";
+
+			if(childNoteValue) {
+				let childNote = new Note(childNoteValue, this);
+				childNote.addNote();
+			}
+		});
 	}
 }
