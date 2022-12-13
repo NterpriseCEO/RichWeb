c0035c337ad073e57676ee671e9917fcf15fa644
NterpriseCEO <nuzumalex@gmail.com>
2022-12-13 19:51:07 +0000

    Added a colour picker for selecting note colours

--- lab4-notes-app/src/App.js
+++ lab4-notes-app/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import React from 'react';
 
+//notes class
+function Note(props) {
+	return (
+		<pre className="note" style={{ backgroundColor: props.colour }}>
+			{props.note}
+		</pre>
+	);
+}
+
 class Panel extends React.Component {
 	textInput = "";
-	
+	colourValue = "#ffffff";
+
 	values = [];
 
 	constructor(props) {
@@ -19,6 +29,7 @@ class Panel extends React.Component {
 		return (
 			<div className="panel">
 				<textarea onChange={this.changeValue}></textarea>
+				<input type = "color" onChange={this.changeColour} defaultValue = "#ffffff"></input>
 				<button onClick={() => this.addNote()}>Add note</button>
 				<div className="notes">
 					{this.state.displayNotes}
@@ -32,18 +43,23 @@ class Panel extends React.Component {
 		this.textInput = event.target.value;
 	}
 
+	changeColour = event => {
+		console.log(event.target.value);
+		this.colourValue = event.target.value;
+	}
+
 	addNote() {
 		//Adds a note to the notes array if the textInput value is not empty
 		if(this.textInput === "" || this.textInput === null) {
 			return;
 		}
 		//Pushes the textInput value to the notes array
-		this.state.notes.push(this.textInput);
+		this.state.notes.push({note: this.textInput, colour: this.colourValue});
 		this.textInput = "";
 		//Updates the displayNotes array with the contents of the notes array
 		this.setState({
 			displayNotes: this.state.notes.map((note, index) => {
-				return <pre key={index}>{note}</pre>
+				return <Note key={index} note = {note.note} colour = {note.colour}/>
 			})
 		});
 	}
