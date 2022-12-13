07101e6900ee5f3189906d45135f935c50843ede
NterpriseCEO <nuzumalex@gmail.com>
2022-12-13 18:54:56 +0000

    Can add notes to the screen

--- lab4-notes-app/src/App.js
+++ lab4-notes-app/src/App.js
@@ -1,23 +1,51 @@
 import './App.css';
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
 
 class Panel extends React.Component {
-	notes = [];
+	textInput = "";
+	
+	values = [];
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			notes: [],
+			displayNotes: []
+		}
+	}
 
 	render() {
 		return (
 			<div className="panel">
-				<textarea></textarea>
+				<textarea onChange={this.changeValue}></textarea>
 				<button onClick={() => this.addNote()}>Add note</button>
+				<div className="notes">
+					{this.state.displayNotes}
+				</div>
 			</div>
 
 		);
 	}
 
+	changeValue = event => {
+		this.textInput = event.target.value;
+	}
+
 	addNote() {
-		this.notes.push("hello");
-		console.log(this.notes);
+		//Adds a note to the notes array if the textInput value is not empty
+		if(this.textInput === "" || this.textInput === null) {
+			return;
+		}
+		//Pushes the textInput value to the notes array
+		this.state.notes.push(this.textInput);
+		this.textInput = "";
+		//Updates the displayNotes array with the contents of the notes array
+		this.setState({
+			displayNotes: this.state.notes.map((note, index) => {
+				return <pre key={index}>{note}</pre>
+			})
+		});
 	}
 }
 
