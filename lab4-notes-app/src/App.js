1ab5087bc544cf92a0acc0b91a28b3663a266d9c
NterpriseCEO <nuzumalex@gmail.com>
2022-12-13 04:45:48 +0000

    Started work on lab 4

--- lab4-notes-app/src/App.js
+++ lab4-notes-app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+class Panel extends React.Component {
+	notes = [];
+
+	render() {
+		return (
+			<div className="panel">
+				<textarea></textarea>
+				<button onClick={() => this.addNote()}>Add note</button>
+			</div>
+
+		);
+	}
+
+	addNote() {
+		this.notes.push("hello");
+		console.log(this.notes);
+	}
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Panel/>
+		</div>
+	);
 }
 
 export default App;
